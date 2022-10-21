0bce1511c62df68c7fe54931880517c0225bb26f
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2022-10-21 13:56:43 -0400

    a6 lab

---
+++ src/labs/a6/index.js
@@ -0,0 +1,17 @@
+import Nav from "../../nav";
+import Classes from "./classes";
+import Styles from "./styles";
+import TodoList from "./todo/todo-list";
+
+function Assignment6() {
+    return (
+        <div>
+            <h1>Assignment 6</h1>
+            <Nav/>
+            <Classes/>
+            <Styles/>
+            <TodoList/>
+        </div>
+    );
+}
+export default Assignment6;
